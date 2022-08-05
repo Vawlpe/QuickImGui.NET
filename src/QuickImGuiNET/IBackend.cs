381f3d6cc342a01b73d3ac7e5a0b396c169b7613
Vawlpe <vawlpe@gmail.com>
2022-08-05 22:04:33 +0300

    Backend & Texture abstractions
    VAW-100 VAW-101

---
+++ src/QuickImGuiNET/IBackend.cs
@@ -0,0 +1,22 @@
+using ImGuiNET;
+
+namespace QuickImGuiNET;
+
+public abstract class Backend
+{
+    public abstract void Run(
+        Action  DrawUICallback,
+        float   deltaSeconds        = 1f / 60f,
+        Action? UpdateCallback      = null,
+        Action? RenderCallback      = null,
+        Action? EarlyUpdateCallback = null,
+        Action? EarlyRenderCallback = null
+    );
+    public abstract void RenderImDrawData(ImDrawDataPtr drawData);
+    public abstract IntPtr BindTexture(Texture texture);
+    public abstract IntPtr UpdateTexture(Texture texture);
+    public abstract void FreeTexture(IntPtr ID);
+    public abstract void UpdateInput(dynamic input);
+    public Dictionary<IntPtr, dynamic> Textures = new();
+    public (IntPtr ID, dynamic Texture) FontTexture;
+}
