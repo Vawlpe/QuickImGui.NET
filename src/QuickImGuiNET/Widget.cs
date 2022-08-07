aee1c944eaec6a627afbe019fe53b7398f4e715a
Vawlpe <vawlpe@gmail.com>
2022-08-07 09:03:43 +0300

    VAW-91 Widget system
    VAW-101

---
+++ src/QuickImGuiNET/Widget.cs
@@ -0,0 +1,103 @@
+using ImGuiNET;
+using System.Numerics;
+
+namespace QuickImGuiNET;
+
+public abstract class Widget
+{
+    public string Name = String.Empty;
+    public Vector2 Size = Vector2.Zero;
+    public Vector2 Position = Vector2.Zero;
+    public ImGuiCond PositionCond = ImGuiCond.Always;
+    public ImGuiCond SizeCond = ImGuiCond.Always;
+    public bool Visible = false;
+    public WidgetRenderMode RenderMode = WidgetRenderMode.Raw;
+    public abstract void RenderContent();
+    public void Render(bool? border = null, ImGuiWindowFlags? flags = null)
+    {
+        switch (RenderMode)
+        {
+            case WidgetRenderMode.Raw:
+                RenderContent();
+                break;
+
+            case WidgetRenderMode.Window:
+                RenderInWindow(flags ?? ImGuiWindowFlags.None);
+                break;
+            
+            case WidgetRenderMode.Child:
+                RenderInChild(border ?? false, flags ?? ImGuiWindowFlags.None);
+                break;
+
+            case WidgetRenderMode.Popup:
+                RenderInPopup(flags ?? ImGuiWindowFlags.None);
+                break;
+        };
+    }
+    public void RenderInChild(bool border, ImGuiWindowFlags flags = ImGuiWindowFlags.None)
+    {
+        ImGui.SetCursorPos(Position);
+        if (!Visible || !ImGui.BeginChild(Name, Size, border, flags))
+            return;
+
+        RenderContent();
+        ImGui.EndChildFrame();
+    }
+    public void RenderInWindow(ImGuiWindowFlags flags = ImGuiWindowFlags.None)
+    {
+        ImGui.SetNextWindowSize(Size, SizeCond);
+        ImGui.SetNextWindowPos(Position, PositionCond);
+        if (!Visible || !ImGui.Begin(Name, ref Visible, flags))
+            return;
+
+        RenderContent();
+        ImGui.End();
+    }
+    public void RenderInPopup(ImGuiWindowFlags flags = ImGuiWindowFlags.None)
+    {
+        if (Visible && !ImGui.IsPopupOpen(Name))
+            ImGui.OpenPopup(Name);
+
+        ImGui.SetNextWindowSize(Size, SizeCond);
+        ImGui.SetNextWindowPos(Position, PositionCond);
+        if (!Visible || !ImGui.BeginPopup(Name, flags))
+            return;
+
+        if (!Visible && ImGui.IsPopupOpen(Name))
+            ImGui.CloseCurrentPopup();
+
+        RenderContent();
+        ImGui.EndPopup();
+    }
+
+    public void Open()
+    {
+        Visible = true;
+        OnOpen(this);
+    }
+    public void Close()
+    {
+        Visible = false;
+        OnClose(this);
+    }
+    public void Toggle()
+    {
+        Visible = !Visible;
+        if (Visible)
+            OnOpen(this);
+        else
+            OnClose(this);
+        OnToggle(this);
+    }
+    public event Action<Widget> OnOpen = delegate { };
+    public event Action<Widget> OnClose = delegate { };
+    public event Action<Widget> OnToggle = delegate { };
+}
+
+public enum WidgetRenderMode
+{
+    Raw,
+    Window,
+    Child,
+    Popup
+}
--- src/examples/QuickImGuiNET.Example.Veldrid/Program.cs
+++ src/examples/QuickImGuiNET.Example.Veldrid/Program.cs
@@ -1,31 +1,54 @@
-﻿using ImGuiNET;
+using ImGuiNET;
+using System.Numerics;
 using VRBK = QuickImGuiNET.Veldrid;
 
 namespace QuickImGuiNET.Example.Veldrid;
 
 public class Program
 {
-    public static readonly string[] defaultArgs = new string[] { "640", "480", "-1" };
+    public static readonly string[] defaultArgs = new string[] { "1280", "720", "-1" };
+    public static VRBK.Backend backend;
     public static void Main(string[] args)
     {
-        if (args.Length != 3)
-        {
-            Console.WriteLine("Invalid arguments. Usage: QuickImGuiNET.Example.Veldrid.exe <width> <height> <veldridBacknedIndex>");
-            args = defaultArgs;
-        }
+        if (args.Length != defaultArgs.Length)
+            args = args.Concat(defaultArgs.Skip(args.Length)).ToArray();
         int width = int.Parse(args[0]);
         int height = int.Parse(args[1]);
         int veldridBackendIndex = int.Parse(args[2]);
 
-        var backend = new VRBK.Backend(width, height, veldridBackendIndex);
+        backend = new VRBK.Backend(width, height, veldridBackendIndex);
+        
+        widgets.Add(new ExampleWidget()
+        {
+            Visible = true,
+            RenderMode = WidgetRenderMode.Window,
+            Name = "ExampleWidget##example001",
+            Position = new Vector2(100, 100),
+            Size = new Vector2(150, 200),
+            SizeCond = ImGuiCond.FirstUseEver,
+            PositionCond = ImGuiCond.FirstUseEver
+        });
+        widgets.Add(new ExampleWidget()
+        {
+            Visible = true,
+            RenderMode = WidgetRenderMode.Window,
+            Name = "ExampleWidget##example002",
+            Position = new Vector2(100, 310),
+            Size = new Vector2(275, 320),
+            SizeCond = ImGuiCond.FirstUseEver,
+            PositionCond = ImGuiCond.FirstUseEver,
 
-        backend.Run(DrawUI);
+            IconSizeMult = 0.25f
+        });
+
+        backend.Run(Draw);
     }
 
     public static bool showDemoWindow = false;
-    public static void DrawUI()
+    public static List<Widget> widgets = new();
+    public static void Draw()
     {
-        ImGui.DockSpaceOverViewport(ImGui.GetMainViewport());
+        ImGui.DockSpaceOverViewport();
         if (ImGui.BeginMainMenuBar())
         {
             ImGui.MenuItem("Show Demo Window", "", ref showDemoWindow);
@@ -35,10 +58,7 @@ public class Program
         if (showDemoWindow)
             ImGui.ShowDemoWindow(ref showDemoWindow);
 
-        if (!ImGui.Begin("QuickImGuiNET.Example.Veldrid"))
-            return;
-
-        ImGui.Text("Hello World!");
-        ImGui.End();
+        foreach (var widget in widgets)
+            widget.Render();
     }
 }
